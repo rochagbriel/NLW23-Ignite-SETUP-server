e762d3f2c51a4b864a0440ba8df82964c19c3dff
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-18 21:58:32 +0100

    end day 2

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,81 @@
+import dayjs from "dayjs"
 import { FastifyInstance } from "fastify"
+import { z } from 'zod'
 import { prisma } from "./lib/prisma"
 
 
 export async function appRoutes(app: FastifyInstance) {
-    app.get('/hello', async () => {
-        const habits = await prisma.habit.findMany()
-    
-        return habits
-    })
+    app.post('/habits', async (request) => {
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+            )
+        })
+        
+        const { title, weekDays } = createHabitBody.parse(request.body)
 
+        const today = dayjs().startOf('day').toDate()
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: today,
+                weekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay,
+                        }
+                    })
+                }
+            }
+        })
+    })
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
     
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDote = dayjs(date).startOf('day')
+        const weekDay = parsedDote.get('day')
+
+        // All possible habits
+        // Completed habits
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDote.toDate(),
+            },
+            include: {
+                dayHabits: true,
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits,
+        }
+    })
 }
+
+
 
