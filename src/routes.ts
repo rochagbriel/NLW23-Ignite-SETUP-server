e0278906c09fdfd21dcf4c123152be80b6e8c253
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-18 19:37:30 +0100

    eng files

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "./lib/prisma"
+
+
+export async function appRoutes(app: FastifyInstance) {
+    app.get('/hello', async () => {
+        const habits = await prisma.habit.findMany()
+    
+        return habits
+    })
+
+    
+}
+
