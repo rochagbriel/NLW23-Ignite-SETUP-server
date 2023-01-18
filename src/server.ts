eff792005340fa5937a3e0bd5bd6948c3c26d4b2
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-18 03:31:39 +0100

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+/**
+ * HTTP Method: Get, Posst, Put, Patch, Delete
+ */
+
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany()
+
+    return habits
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running!')
+})
