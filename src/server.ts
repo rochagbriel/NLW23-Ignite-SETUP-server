478ffabb1e8bb0a7e882f30046e4cd6bae964993
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-22 19:22:55 +0100

    Bug fix

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { appRoutes } from './routes'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-
-/**
- * HTTP Method: Get, Posst, Put, Patch, Delete
- */
 
 app.register(cors)
 app.register(appRoutes)
 
 app.listen({
-    port: 3333,
+  port: 3333,
+  host: '0.0.0.0',
 }).then(() => {
-    console.log('HTTP Server running!')
+  console.log('HTTP Server running!')
 })
